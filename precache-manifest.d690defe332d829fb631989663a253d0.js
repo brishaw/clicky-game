8905c0a5ef34ba369de1876ea1a311c47d126ed3
Brian <40767783+brishaw@users.noreply.github.com>
2018-12-15 09:58:33 -0500

    Updates

---
+++ precache-manifest.d690defe332d829fb631989663a253d0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "eeafe898c5efc6c4e6a1",
+    "url": "/clicky-game/static/js/main.eeafe898.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/clicky-game/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "eeafe898c5efc6c4e6a1",
+    "url": "/clicky-game/static/css/main.55b811de.chunk.css"
+  },
+  {
+    "revision": "9f2007e31532084c7c1b601e4ffca835",
+    "url": "/clicky-game/index.html"
+  }
+];
